ee40a4dc5389036fb85bb3270bcd2352973f56f6
github username <github email address>
2017-12-05 15:25:05 -0500

    added relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_one :job
+  has_one :home
+  has_many :user_dogs
+  has_many :dogs, through: :user_dogs
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205170118) do
+ActiveRecord::Schema.define(version: 20171205202037) do
 
   create_table "dogs", force: :cascade do |t|
     t.string "personality"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20171205170118) do
     t.boolean "pets"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_homes_on_user_id"
   end
 
   create_table "jobs", force: :cascade do |t|
