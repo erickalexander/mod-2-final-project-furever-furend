024831d4e287535b6e0a56a4ae49fc22a97abc97
github username <github email address>
2017-12-07 10:46:51 -0500

    small additions

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   get "/signin", to: "sessions#new", as: 'signin'
   post "/sessions", to: "sessions#create", as: "sessions"
   delete "/sessions", to: "sessions#destroy", as: "signout"
+  root "users#index"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    it "should assert that 2 == 2" do
-      expect(2).to eq(2)
-    end
+
+  let(:home) do
+    Home.create(
+      city_or_suburb: 'city',
+      apt_or_house: 'house',
+      num_of_rooms: 1,
+      num_of_occupants: 2,
+      kids: true,
+      pets: false,
+      user_id: kat.id
+    )
+  end
+
+  let(:jobs) do
+    Job.create(
+      num_hours_at_work: 6,
+      salary: 30000
+    )
+  end
+
+  let(:kat) {User.create(username: 'kpop', password_digest: 'beef', job_id: jobs.id, home_id: home.id)}
+
+  it 'has a username' do
+    expect(kat.username).to eq('kpop')
+  end
+
 end
