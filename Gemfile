a72f7eaba7caea01de412b5e47c735f85ee891e7
erickalexander <erickcamacho1@gmail.com>
2018-02-26 16:00:13 -0500

    fixed a bug in the point system to choose dogs, changed database from sqlite3 to postgresql (heroku)

--- Gemfile
+++ Gemfile
@@ -9,7 +9,8 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# gem 'sqlite3'
+gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
@@ -44,7 +45,7 @@ group :development, :test do
   gem 'selenium-webdriver'
   gem 'rspec-rails', '~> 3.6'
   gem 'database_cleaner'
-
+  gem 'sqlite3'
 end
 
 group :development do
@@ -54,6 +55,10 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+group :production do
+  gem 'pg'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,22 +19,22 @@ class User < ApplicationRecord
   def assign_job_points
     points = 0
     x = Job.find(self.job_id).salary
-    case x
-    when x < 25000
+    if x <= 25000
       points += 1
-    when x < 70000
+    elsif x > 25000 && x <= 70000
       points += 2
     else
       points += 3
     end
     y = Job.find(self.job_id).num_hours_at_work
-    case y
-    when y < 7
+    if y < 7
       points += 3
     else
       points -= 3
     end
-    points = 0 if points < 0
+    if points < 0
+      points = 0
+    end
     points
 
   end
@@ -42,43 +42,37 @@ class User < ApplicationRecord
   def assign_home_points #to be edited
     points = assign_job_points
     x = Home.find(self.home_id)
-    case x
-    when x.apt_or_house.downcase == "apartment"
+    if x.apt_or_house.downcase == "apartment"
       points += 2
     else
       points += 4
     end
     y = Home.find(self.home_id)
-    case y
-    when y.city_or_suburb.downcase == "suburb"
+    if y.city_or_suburb.downcase == "suburb"
       points += 4
     else
       points += 2
     end
     z = Home.find(self.home_id)
-   case z
-   when z.num_of_rooms < 2
+    if z.num_of_rooms < 2
       points += 1
     else
       points += 2
     end
     a = Home.find(self.home_id)
-   case a
-   when a.num_of_occupants < 2
+    if a.num_of_occupants < 2
       points += 1
     else
       points += 2
     end
     b = Home.find(self.home_id)
-   case b
-   when b.kids == false
+    if b.kids == nil
       points += 1
     else
       points += 2
     end
     c = Home.find(self.home_id)
-   case c
-   when c.pets == false
+    if c.pets == nil
       points += 1
     else
       points += 2
@@ -87,14 +81,13 @@ class User < ApplicationRecord
   end
 
   def get_list_of_dogs
-    # byebug
-    if (1 <= self.assign_home_points) && (self.assign_home_points <= 8)
+    if (self.assign_home_points >= 1) && (self.assign_home_points <= 8)
       Dog.where("size = ? and (barking_level = ? or barking_level = ?)  and (energy_level = ? or energy_level = ?)", 's', 1, 2, 'Not very active','Somewhat active')
 
-    elsif (8 <= self.assign_home_points) && (self.assign_home_points <= 14)
+    elsif (self.assign_home_points >= 9) && (self.assign_home_points <= 14)
       Dog.where("(size = ? or size = ?) and (barking_level = ? or barking_level = ?)  and (energy_level = ? or energy_level = ?)", 's', 'm', 1, 2, 'Very active','Somewhat active')
 
-    elsif (15 <= self.assign_home_points) && (self.assign_home_points <= 20)
+    elsif (self.assign_home_points >= 15) && (self.assign_home_points <= 22)
       Dog.where("(energy_level = ? or energy_level = ?) and children_friendly =? ", 'Very active','Somewhat active', true)
     end
   end
